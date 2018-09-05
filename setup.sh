8d87347e59b7275b3e311127f5f71a709993f2a4
Daniel Öhrlund <danielohrlund@gmail.com>
2018-09-05 20:11:31 +0200

    update setup script to use neovim stable ppa instead of unstable

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Install neovim
 sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
 
